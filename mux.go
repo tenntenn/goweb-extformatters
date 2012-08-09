4860b1ce90a0cf2d786293f254562c451af58b0f
Takuya Ueda <uedatakuya@gmail.com>
2012-08-10 01:01:33 +0900

    Create mux formatter.

---
+++ mux.go
@@ -0,0 +1,69 @@
+/*
+A plugin of goweb which provides extra formatters and decoders.
+https://github.com/tenntenn/goweb-extformatters
+
+Copyright (c) 2012, Takuya Ueda. 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+* Neither the name of the author nor the names of its contributors may be used
+  to endorse or promote products derived from this software
+  without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package extformatters
+
+import (
+    "code.google.com/p/goweb/goweb"
+    "strings"
+)
+
+type FormatterMux []goweb.Formatter
+
+func (mux FormatterMux) Format(cx *goweb.Context, input interface{}) (output []uint8, err error) {
+    output = input
+    for _, f := range mux {
+        output, err = f.Format(cx, output)
+        if err != nil {
+            return nil, err
+        }
+    }
+
+    return output, nil
+}
+
+var MuxFormatStringDelimita string = "+"
+
+func (mux FormatterMux) Match(cx *goweb.Context) bool {
+    formatStrings := strings.Split(strings.ToUpper(cx.Format), MuxFormatStringDelimita)
+    cpyCx := &Context{cx.Request, cx.ResposeWriter, cx.PathParams, cx.Format}
+    for i, f := range mux {
+        cpyCx.Format = formatStrings[i]
+        if !f.Match(cpyCx) {
+            return false
+        }
+    }
+    return true
+}
+
+func MuxFormatString(formats ...string) string {
+    return strings.Join(formats, MuxFormatStringDelimita)
+}
